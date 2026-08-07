d8bc74fadf728d181d1115c682a9f7346686c305
Indura7 <160689278+Indura7@users.noreply.github.com>
2026-08-07 12:56:13 +0530

    Add Link component to ArtworkCard mapping for navigation

--- src/app/artworks/page.tsx
+++ src/app/artworks/page.tsx
@@ -1,6 +1,7 @@
 
 import { supabase } from '@/lib/supabase'; 
 import ArtworkCard from "@/components/modules/artworks/artworkcard";
+import Link from "next/link";
 
 
 export default async function ArtworksPage() {
@@ -17,12 +18,20 @@ export default async function ArtworksPage() {
     <div className="container mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6"> Explore Artworks</h1>
       
-      {/* 2. Map over database rows and pass them to your component */}
+      
+      
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {artworks?.map((item) => (
+          <Link key={item.art_id} href={`/artworks/${item.art_id}`}>
           <ArtworkCard key={item.art_id} artwork={item} />
+          </Link>
         ))}
       </div>
+
+      
+
+
     </div>
   );
 }
