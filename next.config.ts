ab3ac285ec84a054c41f836ec2bdced29b959a77
NGayanuka <gayanuka2004@gmail.com>
2026-08-18 10:44:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
     ],
   },
 };
