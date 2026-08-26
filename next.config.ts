f02a1b7a897cb75f423298cd965af356acb021ec
Sandali Githara <2024t02197@stu.cmb.ac.lk>
2026-08-27 00:21:12 +0530

    Fix next.config.ts merge conflict

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'vknetjgebapvlncovblp.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-    ],
-  },
-=======
-/*
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactCompiler: true,
->>>>>>> workshop
-};
-
-export default nextConfig; */
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -40,6 +7,16 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "vknetjgebapvlncovblp.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
     ],
   },
