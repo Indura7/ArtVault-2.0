6bc5a961b1474e2708afa8ef064663a6b20b5a9b
Indura7 <indura2020new@gmail.com>
2026-09-10 00:32:20 +0530

    card

--- src/app/artworks/[id]/page.tsx
+++ src/app/artworks/[id]/page.tsx
@@ -33,7 +33,7 @@ export default async function ArtworkDetailPage({ params }: PageProps) {
       <p>Artwork Detail Page </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         
-        <div>
+        <div>   
           <Zoom
             src={artwork.image_path}
             alt={artwork.title}
